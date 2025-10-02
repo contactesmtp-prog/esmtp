8bcb1f793637bb4b86bf861fcf12c5fc4c707ffe
Sef-Hk <ahk.sefsaf@gmail.com>
2025-10-02 15:21:28 +0100

    run from the ghost

--- redirects.js
+++ redirects.js
@@ -1,20 +1,46 @@
+// const redirects = async () => {
+//   const internetExplorerRedirect = {
+//     destination: '/ie-incompatible.html',
+//     has: [
+//       {
+//         type: 'header',
+//         key: 'user-agent',
+//         value: '(.*Trident.*)', // all ie browsers
+//       },
+//     ],
+//     permanent: false,
+//     source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
+//   }
+
+//   const redirects = [internetExplorerRedirect]
+
+//   return redirects
+// }
+
+// export default redirects
+
+// /Users/zix/Desktop/ZINOU-PROJECT/esmtp/redirects.js
 const redirects = async () => {
+  // 1) Strip "/home" for supported locales
+  const stripHome = [
+    { source: '/:lang(fr|en|ar)/home', destination: '/:lang', permanent: true },
+    { source: '/:lang(fr|en|ar)/home/', destination: '/:lang', permanent: true },
+  ]
+
+  // 2) Your existing Internet Explorer redirect
   const internetExplorerRedirect = {
     destination: '/ie-incompatible.html',
     has: [
-      {
-        type: 'header',
-        key: 'user-agent',
-        value: '(.*Trident.*)', // all ie browsers
-      },
+      { type: 'header', key: 'user-agent', value: '(.*Trident.*)' }, // IE only
     ],
     permanent: false,
-    source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
+    source: '/:path((?!ie-incompatible.html$).*)',
   }
 
-  const redirects = [internetExplorerRedirect]
-
-  return redirects
+  return [
+    ...stripHome, // put ours first
+    internetExplorerRedirect,
+  ]
 }
 
 export default redirects
