20d3016b75e90e79432489f9138a788bc7251496
Sef-Hk <ahk.sefsaf@gmail.com>
2025-10-13 21:52:26 +0100

    the laszt logo without BG

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,7 +17,7 @@ export const Logo = ({ loading = 'lazy', priority = 'low', className }: Props) =
       fetchPriority={priority}
       decoding="async"
       className={clsx('h-10 w-auto object-contain', className)}
-      src="/1000057904.png"
+      src="/lastoneSure.png"
     />
   )
 }
